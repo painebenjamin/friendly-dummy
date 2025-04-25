12e8bfa9437093bc72414a569c903065109b96da
Benjamin Paine <57536852+painebenjamin@users.noreply.github.com>
2025-04-25 18:38:12 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="friendlydummy",
+    name="friendly-dummy",
     version="0.0.1",
     author="Dummy Author",
     description="A dummy package that prints hello world",
